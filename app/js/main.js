1b934d9f9d0e412f36e86ef9128979873cc032da
Джанаев Азат <osycle@gmail.com>
2017-11-14 17:40:22 +0500

    14.11

--- app/js/main.js
+++ app/js/main.js
@@ -43,63 +43,61 @@ $(function(){
 		AOS.refresh()
 	}, 300);
 
-	//Owl.Carousel
 
-  	$(".index-carousel").owlCarousel({
-		    animateOut: 'fadeOut',
-		    animateIn: 'fadeIn',
-		    //autoWidth: true,
-		    //rewind: true,
-		    mergeFit: true,
-		    center: true,
-		    loop: true,
-		    items:1,
-		    //margin:30,
-		    //mouseDrag: false,
-		    autoplay: true,
-		    autoplayTimeout: 3000,
-		    smartSpeed:450
-		});
+	//Owl.Carousel
+	$(".index-carousel").owlCarousel({
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    //autoWidth: true,
+	    //rewind: true,
+	    mergeFit: true,
+	    center: true,
+	    loop: true,
+	    items:1,
+	    //margin:30,
+	    //mouseDrag: false,
+	    autoplay: true,
+	    autoplayTimeout: 3000,
+	    smartSpeed:450
+	});
+
+  // Flikity Carousel
+  	var arrowStyle = { 
+		  x0: 10,
+		  x1: 60, y1: 50,
+		  x2: 60, y2: 45,
+		  x3: 15
+		}
+  // PARTNERS
+	var carouselPartners = $('.index-carousel-advert').flickity({
+		autoPlay: 2000,
+		arrowShape: arrowStyle,
+		//imagesLoaded: true,
+		//prevNextButtons: false,
+		//wrapAround: true,
+		//selectedAttraction: 0.2,
+		//friction: 0.2,
+		//rightToLeft: true,
+		pageDots: false,
+		contain: true,
+		percentPosition: true,
+		cellAlign: 'center'
+	});
+
+
+
+
 	
 
 
 
-
-
-
-	var header_status = false;
-	var statusSearchView = true;
-
-
 	//menu init
-	$(".nav-menu").initMenu({
-		"menuToggleBtn": ".menu-toggle",
-		"subMenu": ".sub-menu-1",
-		"modalMenu": "#menuModal",
-		menuHoverIn: function(){}
+	$(".vi-container").initMenu({
+		"menuToggleBtn": ".menu-toggle"
 	})
 
 
-function phoneDap(){
-	if ( checkView(992) )
-		return;
-	$(".copyright")
-		.before( $(".social") )
-}
-
-function dropbtn(){
-	$(".dropdown-lang .dropbtn").on("click", function(){
-		$( $(this).siblings(".dropdown-content") ).toggleClass("active");
-	});
-	$(".dropdown-lang .dropdown-content").on("mouseleave", function(){
-		$(this).removeClass("active");
-	});
-}
-dropbtn();
-
-
-
-
+	var statusSearchView = true;
 $(".btn-search").on("click", function(){
 	$(this).find("i")
 		.toggleClass("fa fa-search")
@@ -133,67 +131,31 @@ $(".btn-search").on("click", function(){
 
 
 //RESIZE
-$( window ).on("resize", function(e){
-
-	// body
-
-});
+$( window ).on("resize", function(e){});
 
 //SCROLL
+var header_status = false;
 $( window ).on("scroll", function(e){
-	
 	if($(window).scrollTop() > 300 && header_status == false){
-		
 		header_status = true; 
-
 	}else if($(window).scrollTop() < 300 && header_status == true){
-
 		header_status = false;
-
 	}
-
 });
 
 
+	$("#fs-range").on( "input", function(e){
+		console.log(e);
+	})
 
 
 
 
- $.fn.fadeToggleBool = function( dura = 290 ){
- 	var self = $( $(this) ),
- 		 bool = self.css("display") == "none";
-
-	self.fadeToggle({
-
-		duration: dura,
-		easing: "linear"
-
-	});
-
-	return bool;
- }
-
-
-setInterval( function(){
-	bannerImgToggle();
-}, 6000 );
 
 	});//$
 }) (jQuery);
 
 
-
-var bannerBg = 1;
-function bannerImgToggle(){
-	var banner = $( $("#banner") ) ;
-
-	window.bannerSlideBg_1 = banner.attr('style');
-	window.bannerSlideBg_2 = banner.attr('data-toggle-img');
-
-	banner.attr("data-toggle-img", bannerSlideBg_1);
-	banner.attr("style", bannerSlideBg_2);
-
-}
 
 
 function Menu( menu, options ){
@@ -201,114 +163,17 @@ function Menu( menu, options ){
 	var self = this;
 	menu = $( menu );
 
-	//ПОЛЯ
-	this.menuClass						= menu[0].className;
-	this.menuToggleBtn 				= $( $(options.menuToggleBtn) ) ;
-	this.menuToggleBtnParent  = $(this.menuToggleBtn).parent();
-	this.subMenu 							= $( menu.find(options.subMenu) );
-	this.modalMenu  					= $( $(options.modalMenu) );
-	this.modalMenuStatus 		 	= false; 
+	console.log( menu, options );
 
-	//МЕТОДЫ
-	this.menuToggle					= function(){
-		$( this.menuToggleBtn ).trigger("click");
-		return this.modalMenuStatus = !this.modalMenuStatus;
-	}
-
-
-	menu.find("[class|='sub']").closest("li").addClass("sub-parent");
-	$(".min-navbar").append( menu.clone() ).find( "."+this.menuClass ).addClass("min");
-
-	this.modalMenu.on('show.bs.modal', function (e) {
-		if (self.modalMenu.length === 0) return;
-
-		options.modalMenuShow();
-
-	})
-	this.modalMenu.on('shown.bs.modal', function (e) {
-		if (self.modalMenu.length === 0) return;
-
-		options.modalMenuShown();
-		
-	})
-	this.modalMenu.on('hide.bs.modal', function (e) {
-		if (self.modalMenu.length === 0) return;
-		options.modalMenuHidden();
-
-	})
-	this.modalMenu.on('hidden.bs.modal', function (e) {
-		if (self.modalMenu.length === 0) return;
-		self.menuToggleBtn.addClass("collapsed");
-		options.modalMenuHidden();
-
-	})
-
-	//HOVER MENU
-	menu.find("li").hover(
-		function(){
-			options.menuHoverIn();
-	},function(){
-			options.menuHoverOut();
-	});
-
-
-
-	//HOVER SUB-MENU
-	this.subMenu.hover(
-		function(){
-			adposmenu(this);
-			options.subHoverIn();
-		},
-		function(){
-			options.subHoverOut();
-		});
-
-
-
-
-	this.menuToggleBtn.on("click", function(){
-		if(this.modalMenuStatus)
-			$(this).addClass("collapsed");
-		else
-			$(this).removeClass("collapsed");
-
-		options.menuToggle();
-	});
-	
-
-	//	FUNCITON
-
-	function adposmenu(subMenu){
-		//Адаптация положение подменю в зависимости от размера экрана
-		var el = $(subMenu).find("li ul");
-		if (el.length === 0) return;
-		if ( $( window ).width() < el.width() + el.offset().left ) 
-			el.addClass("left");
-	}
 
 }
-
-
-
 
 
 window.$.fn.initMenu = function(option){
 
 	var options = $.extend({
-		"menuToggleBtn"					: false, 		// Кнопка бара
-		"subMenu"								: false, 		// Класс подменю
-		"modalMenu"							: false, 		// Модальное меню
 
-		"menuToggle"						: Function, 	// Переключение
-		"menuHoverIn"						: Function,
-		"menuHoverOut"					: Function,
-		"subHoverIn"						: Function,
-		"subHoverOut"						: Function,
-		"modalMenuShow"					: Function, 	// Открытие меню
-		"modalMenuShown"				: Function,		// Меню раскрыт
-		"modalMenuHide"					: Function, 	// Раскрытие Меню
-		"modalMenuHidden"				: Function 		// Меню скрыт
-
+		viContent: false
 
 	}, option );
 	var menu = new Menu(this, options);
